2ae92c420aca04c2acf2c586b05fc7d397d69f02
DiskElemental <jsbrinkman@gmail.com>
2017-02-11 00:06:02 -0600

    Fixed minor issues with GameObjects implementation, updated the EA to work with new format. Implemented recombination for both Games and Game Objects, as detailed in google drive. TODO: Connect to MySQL database, implement Mutation and Generation iteration.

--- EA/GameObjects.py
+++ EA/GameObjects.py
@@ -10,52 +10,39 @@ class Game(object):
     Score = 0
 
     #Basic Properties to create a game, everything else will be generated later
-    def __init__(self, OffScreenEffect, NumActionButtons, Score):
+    def __init__(self, OffScreenEffect, NumActionButtons, Score, objList, NumObj):
         self.ID = random.randint(0,2**(128)) #Sample Space so big collisions aren't a concern
         self.OffScreenEffect = OffScreenEffect
         self.NumActionButtons = NumActionButtons
         self.Score = Score
-        self.ObjectList = [] # This is the list of GameObject instances
-
-
-    def populateObjectsList(self, numObj):
-    	for i in range(numObj):
-    		# These should be randomized, but I'm a bit lazy at the moment
-    		n = "name"
-    		sh = -1 # a random choice of shape
-    		co = -1 # randomize color
-    		op = -1 # random opacity
-    		# We should probably pass number of action buttons through here for trigger gen
-
-    		newObj = GameObject(n, sh, co, op, -1)
-    		newObj.GenerateReactions
-    		self.ObjectList.append(newObj)
-
-    #We don't pass individual objects into the Game
-    #We define rules for entire classes of objects, as shown below
-    #C = [Number_of_Circles, GameObject_Representing_Circles, [locations_of_circles]]
-    #This allows us to tweak individual object spawn rates
-    #And ensures all objects sharing a sprite have consistent properties.
-    #def SetObjectLogs(self, C, S, T, X, Player):
-    #    self.C = C  #Circles
-    #    self.S = S  #Squares
-    #    self.T = T  #Triangles
-    #    self.X = X  #Crosses
-    #    self.Player = Player    #Player
+        self.ObjectList = objList # This is the list of GameObject instances
+        self.NumObj = NumObj
 
     def SetFitness(self,fitness):
         self.Fitness = fitness
 
+    def __eq__(self, other):
+        if isinstance(other, Game):
+            return(self.Fitness == other.Fitness)
+        return NotImplemented
+    def __gt__(self, other):
+        if isinstance(other, Game):
+            return(self.Fitness > other.Fitness)
+        return NotImplemented
+    def __lt__(self, other):
+        if isinstance(other, Game):
+            return(self.Fitness < other.Fitness)
+        return NotImplemented
+
 
 class GameObject(object):
     Fitness = 0
+    Triggers = []
 
     # A list of action-reaction pairs
     Triggers = []
 
-    # NumActionButtons isn't used here. But I could see it being needed in 
-    # Triggers section
-    def __init__(self, Name, HP, Shape, Color, Opacity, NumActionButtons):
+    def __init__(self, Name, HP, Shape, Color, Opacity):
         self.Name = Name
         self.HP = HP
         self.Shape = Shape
@@ -63,6 +50,7 @@ class GameObject(object):
         self.Opacity = Opacity
 
     def GenerateReactions(self, NumAct):
+        self.Triggers = []
         i = random.randint(1,8)
 
         for trig in range(i):
@@ -73,57 +61,78 @@ class GameObject(object):
     def SetFitness(self, fitness):
         self.Fitness = fitness
 
-    class Trigger:
-    	# Every trigger has one action and one reaction
-    	def __init__(self):
-    		self.action = None
-    		self.reaction = None
 
-    	def GenerateNew(self):
-    		# I know, this makes the search space look huge, but I think it's okay in this case
-    		actionset = ["UpPressed", "LeftPressed", "DownPressed", "RightPressed",
-    					"UpHeld", "LeftHeld", "DownHeld", "RightHeld",
-    					"UpReleased", "LeftReleased", "DownReleased", "RightReleased",
-    					"A1Pressed", "A2Pressed", "A3Pressed", "A4Pressed",
-    					"A1Held", "A2Held", "A3Held", "A4Held",
-    					"A1Released", "A2Released", "A3Released", "A4Released",
-    					"CollideWith"+str(random.randint(1,8)), "CollideWithAny",
-    					"OffScreen"]
-    		# We need to pass in something to figure out what the num of objects in this game is
-    		# to change "CollideWith"+str(random.randint(1,MAXOBJECTS))
+    def __eq__(self, other):
+        if isinstance(other, GameObject):
+            return(self.Fitness == other.Fitness)
+        return NotImplemented
+    def __gt__(self, other):
+        if isinstance(other, GameObject):
+            return(self.Fitness > other.Fitness)
+        return NotImplemented
+    def __lt__(self, other):
+        if isinstance(other, GameObject):
+            return(self.Fitness < other.Fitness)
+        return NotImplemented
 
-    		# Need to put something here to remove Action button triggers for buttons that were left out
-    		# or just only add them conditionally. Not to difficult.
 
-    		shapes = ["Square","Circle","Triangle","Octogon"] # etc. Add more as Unity team adds support
 
-    		reactionset = ["ModScoreBy"+str(random.randint(-100,100)),
-    						"ModSpeedBy"+str(random.randint(-10,10)),
-    						"SetSpeedTo"+str(random.randint(-100,100),
-    						"ModDirBy"+str(random.randint(-180,180),
-    						"SetDirTo"+str(random.randint(0,359),
-    						"DestorySelf", "DestroyObj"+str(random.randint(1,8),
-    						"CreateObj"+str(random.randint(1,8),
-    						"SetShape"+random.choice(shapes),
-    						"ModColorBy"+str(random.randint(-255,255)+"-"+str(random.randint(-255,255)+"-"+str(random.randint(-255,255)]
-    						"SetColorTo"+str(random.randint(0,255)+"-"+str(random.randint(0,255)+"-"+str(random.randint(0,255)
-    						"ModOpacityBy"str(random.randint(-100,100)),
-    						"SetOpacityTo"str(random.randint(0,100)),
-    						"NewLevel", "EndGame"]
-    						# we will add effets for modifying game properties, like off-screen effect, etc.
-    						# but for now you get the picture.
+class Trigger:
+	# Every trigger has one action and one reaction
+    def __init__(self):
+        self.action = None
+        self.reaction = None
 
-    		# Consult Unity team about arbitrary values, see what seems reasonable
-    		# I just put temp values in. The numbers will be related to how Unity
-    		# calulates physics.
+    def __eq__(self, other):
+        if isinstance(other, Trigger):
+            return(self.action == other.action)
+        return NotImplemented
 
-    		# Same note on DestroyObj and CreateObj as Collide above.
-    		# The game will likely control where the new things spawn if a create obj is triggered
-    		
-    		# These strings will be stored in database and parsed Unity-side.
+    def GenerateNew(self):
+		# I know, this makes the search space look huge, but I think it's okay in this case
+        actionset = ["UpPressed", "LeftPressed", "DownPressed", "RightPressed",
+					"UpHeld", "LeftHeld", "DownHeld", "RightHeld",
+					"UpReleased", "LeftReleased", "DownReleased", "RightReleased",
+					"A1Pressed", "A2Pressed", "A3Pressed", "A4Pressed",
+					"A1Held", "A2Held", "A3Held", "A4Held",
+					"A1Released", "A2Released", "A3Released", "A4Released",
+					"CollideWith"+str(random.randint(1,8)), "CollideWithAny",
+					"OffScreen"]
+		# We need to pass in something to figure out what the num of objects in this game is
+		# to change "CollideWith"+str(random.randint(1,MAXOBJECTS))
 
-    		self.action = random.choice(actionset)
-    		self.reaction = random.choice(reactionset)
+		# Need to put something here to remove Action button triggers for buttons that were left out
+		# or just only add them conditionally. Not to difficult.
+
+        shapes = ["Square","Circle","Triangle","Octogon"] # etc. Add more as Unity team adds support
+
+        reactionset = ["ModScoreBy"+str(random.randint(-100,100)),
+						"ModSpeedBy"+str(random.randint(-10,10)),
+						"SetSpeedTo"+str(random.randint(-100,100)),
+						"ModDirBy"+str(random.randint(-180,180)),
+						"SetDirTo"+str(random.randint(0,359)),
+						"DestorySelf", "DestroyObj"+str(random.randint(1,8)),
+						"CreateObj"+str(random.randint(1,8)),
+						"SetShape"+random.choice(shapes),
+						"ModColorBy"+str(random.randint(-255,255))+"-"+str(random.randint(-255,255))+"-"+str(random.randint(-255,255)),
+						"SetColorTo"+str(random.randint(0,255))+"-"+str(random.randint(0,255))+"-"+str(random.randint(0,255)),
+						"ModOpacityBy"+str(random.randint(-100,100)),
+						"SetOpacityTo"+str(random.randint(0,100)),
+						"NewLevel", "EndGame"]
+						# we will add effets for modifying game properties, like off-screen effect, etc.
+						# but for now you get the picture.
+
+		# Consult Unity team about arbitrary values, see what seems reasonable
+		# I just put temp values in. The numbers will be related to how Unity
+		# calulates physics.
+
+		# Same note on DestroyObj and CreateObj as Collide above.
+		# The game will likely control where the new things spawn if a create obj is triggered
+
+		# These strings will be stored in database and parsed Unity-side.
+
+        self.action = random.choice(actionset)
+        self.reaction = random.choice(reactionset)
 
 
 
--- EA/TempEvolutionaryAlgorithm.py
+++ EA/TempEvolutionaryAlgorithm.py
@@ -1,134 +1,161 @@
 from GameObjects import *
 import random, json
 
-def InitializePopulation(Pop_Size, Score, OffScreenEffect, NumObjectTypes, NumObjects, MaxX, MaxY, HP):
+def InitializePopulation(Pop_Size, Score, OffScreenEffect, NumObjects, MaxX, MaxY, HP):
     population = []
-    NumCircle = 0
-    NumSquare = 0
-    NumTriangle = 0
-    NumCross = 0
 
     for i in range(Pop_Size):
+        log = []
         #Pick the number of used buttons, and initialize GameObject
         NumActionButtons = random.randint(2,4)
-        NewGame = Game(OffScreenEffect, NumActionButtons, Score)
 
-        #Randomize the size of each object population
-        for j in range(NumObjects):
-            k = random.randint(1,NumObjectTypes)
-            if(k == 1):
-                NumCircle += 1
-            if(k == 2):
-                NumSquare += 1
-            if(k == 3):
-                NumTriangle += 1
-            if(k == 4):
-                NumCross += 1
-
-        #Begin the ObjectLog as described in the GameObjects File
-        CircleLog = [NumCircle]
-        SquareLog = [NumSquare]
-        TriangleLog = [NumTriangle]
-        CrossLog = [NumCross]
 
         #Initialize all of the GameObjects and properties
-        Circle = GenerateGameObj("Circle", "circle", NumActionButtons, HP)
-        Square = GenerateGameObj("Square", "square", NumActionButtons, HP)
-        Triangle = GenerateGameObj("Triangle", "triangle", NumActionButtons, HP)
-        Cross = GenerateGameObj("Cross", "X", NumActionButtons, HP)
-        Player = GenerateGameObj("Player","player", NumActionButtons, HP)
-        CircleLog += [Circle]
-        SquareLog += [Square]
-        TriangleLog += [Triangle]
-        CrossLog += [Cross]
-        PlayerLog = [1,Player,(random.uniform(0,MaxX),random.uniform(0,MaxY))]
+        for j in range(NumObjects-1):
+            NewLog = [GenerateGameObj(NumActionButtons, HP), (random.randint(0,MaxX),random.randint(0,MaxY))]
+            log.append(NewLog)
+        Player = [GenerateGameObj(NumActionButtons, HP), (random.randint(0,MaxX),random.randint(0,MaxY))] #Using generic methods to generate the player. Need to decide on what specific methods we want later.
+        log.append(Player)
 
-
-
-        #Randomizes starting coordinates for all objects. Saves as list of (X,Y) tuples
-        C_Coords = []
-        for l in range(NumCircle):
-            C_Coords.append((random.uniform(0,MaxX),random.uniform(0,MaxY)))
-        CircleLog += C_Coords
-        S_Coords = []
-        for l in range(NumSquare):
-            S_Coords.append((random.uniform(0,MaxX),random.uniform(0,MaxY)))
-        SquareLog += S_Coords
-        T_Coords = []
-        for l in range(NumTriangle):
-            T_Coords.append((random.uniform(0,MaxX),random.uniform(0,MaxY)))
-        TriangleLog += T_Coords
-        X_Coords = []
-        for l in range(NumCross):
-            X_Coords.append((random.uniform(0,MaxX),random.uniform(0,MaxY)))
-        CrossLog += X_Coords
+        NewGame = Game(OffScreenEffect, NumActionButtons, Score, log, NumObjects)
 
         #Put all the information together, and insert into the Population
-        NewGame.SetObjectLogs(CircleLog,SquareLog,TriangleLog,CrossLog,PlayerLog)
         population.append(NewGame)
 
     return population
 
-def GenerateGameObj(Name, Shape, NumActionButtons, HP):
-    Color = None #TODO: Color Generating Code
+def GenerateGameObj(NumActionButtons, HP):
+    shapes = ["Square","Circle","Triangle","Octogon"] #List can be expanded as unity people gives us more to work with
+    Color = (random.randint(0,255),random.randint(0,255),random.randint(0,255)) #(R, G, B)
     Opacity = random.uniform(0,100) #Opacity Percentage
-    NewObj = GameObject(Name,HP,Shape,Color,Opacity, NumActionButtons)
+    Shape = random.choice(shapes)
+    NewObj = GameObject(Shape+str(Color),HP,Shape,Color,Opacity)
     NewObj.GenerateReactions(NumActionButtons)
     return NewObj
 
+def RecombineGameObject(p1, p2):
+    FinalTrigList = []
+    #1/3 weighted towards lower fitness object, 2/3s to higher object. If equal, then 50/50 split.
+    shapes = [p1.Shape, p2.Shape, max(p1,p2).Shape]
+    Shape = random.choice(shapes)
+
+    #Random HP
+    HP = random.choice([p1.HP, p2.HP])
+
+    #Average of the RGB values
+    Color = ((p1.Color[0]+p2.Color[0])/2, (p1.Color[1]+p2.Color[1])/2, (p1.Color[2]+p2.Color[2])/2)
+
+    print(len(p1.Triggers), len(p2.Triggers))
+
+    MaxTriggers = len(max(p1,p2).Triggers)
+
+    #Average of Opacity
+    Opacity = (p1.Opacity + p2.Opacity)/2
+
+    tmp1 = p1.Triggers
+    tmp2 = p2.Triggers
+
+    #Finds actions the two objects have in common, and ensures that at least one of the reactions for said action is passed on
+    for i in tmp1:
+        for j in tmp2:
+            if(i == j):
+                #1/4 chance of adding both to the trigger list
+                if(random.random() < 0.25 and len(FinalTrigList) <= MaxTriggers):
+                    FinalTrigList.append(i)
+                    FinalTrigList.append(j)
+                else:
+                    #2/3 chance of using the higher fitness trigger
+                    if(random.random() >= 0.33 and len(FinalTrigList) <= MaxTriggers):
+                        if(p1 > p2):
+                            FinalTrigList.append(i)
+                        else:
+                            FinalTrigList.append(j)
+                    elif(len(FinalTrigList) <= MaxTriggers):
+                        if(p1 > p2):
+                            FinalTrigList.append(j)
+                        else:
+                            FinalTrigList.append(i)
+                if(i in tmp1):
+                    tmp1.remove(i)
+                if(j in tmp2):
+                    tmp2.remove(j)
+
+    #fitness proportional selection from remainder of triggers, up to value in range [0, MaxTriggers]
+    fm = float(p1.Fitness + p2.Fitness)
+    if(fm == 0):
+        fm = 0.5
+    NumIterations = random.randint(0, MaxTriggers)
+    while(len(FinalTrigList) < NumIterations):
+        if(random.random() <= p1.Fitness/fm):
+            a = random.choice(tmp1)
+            FinalTrigList.append(a)
+            tmp1.remove(a)
+        else:
+            a = random.choice(tmp2)
+            FinalTrigList.append(a)
+            tmp2.remove(a)
+
+    childObj = GameObject(Shape+str(Color),HP,Shape,Color,Opacity)
+    childObj.Triggers = FinalTrigList
+
+    return childObj
+
+
+def RecombineGame(p1, p2):
+    tmpObjList = []
+    fm = float(p1.Fitness + p2.Fitness)
+    if(fm == 0):
+        fm = 0.5
+    #Fitness Proportional Selection
+    if(random.random() <= p1.Fitness/fm):
+        NumObj = p1.NumObj
+    else:
+        NumObj = p2.NumObj
+
+    #All equal to the highest fitness value
+    NumActionButtons = max(p1,p2).NumActionButtons
+    OffScreenEffect = max(p1,p2).OffScreenEffect
+    Score = max(p1,p2).Score
+
+    for i in range(NumObj):
+        #Parental Selection is k-tournament, with k = 3
+        tmp1 = kTournament(p1.ObjectList,3)
+        tmp2 = kTournament(p2.ObjectList,3)
+        #Coordinates are average of old coordinates
+        NewObj = [RecombineGameObject(tmp1[0],tmp2[0]), ((tmp1[1][0]+tmp2[1][0])/2, (tmp1[1][1]+tmp2[1][1])/2)]
+        tmpObjList.append(NewObj)
+
+    return Game(OffScreenEffect, NumActionButtons, Score, tmpObjList, NumObj)
+
+#Simple k-tournament used for selection of [object, (coordinate)] pairs
+def kTournament(objList, k):
+    best = False
+    for i in range(k):
+        choice = random.choice(objList)
+        if(best != False):
+            if(choice[0] > best [0]):
+                best = choice
+        else:
+            best = choice
+    return best
+
+
+
+
+
+def dumper(obj):
+    return obj.__dict__
+
 def ToJson(CurrentGame):
-    c = CurrentGame.C[1]
-    s = CurrentGame.S[1]
-    t = CurrentGame.T[1]
-    x = CurrentGame.X[1]
-    p = CurrentGame.Player[1]
-
-    CurrentGame.C[1] ="Circle"
-    CurrentGame.S[1] = "Square"
-    CurrentGame.T[1] = "Triangle"
-    CurrentGame.X[1] = "Cross"
-    CurrentGame.Player[1] ="Player"
-
-    #I know it's hideous, but it'll save time in the long run
-    #In the meantime, try not to look at it
-
-    GameDict = {'ID':CurrentGame.ID,'OffScreenEffect':CurrentGame.OffScreenEffect,'Score':CurrentGame.Score,
-    'C':CurrentGame.C,'S':CurrentGame.S,'T':CurrentGame.T,'X':CurrentGame.X,'Player':CurrentGame.Player,
-    'Score':CurrentGame.Score,'Fitness':CurrentGame.Fitness}
-
-    SDict = {'Name':s.Name,'HP':s.HP,'Shape':s.Shape,'Color':s.Color,'Opacity':s.Opacity,'Up':s.Up,
-    'Left':s.Left,'Right':s.Right,'Down':s.Down,'A1':s.A1,'A2':s.A2,'A3':s.A3,'A4':s.A4,'Col_C':s.Col_C,
-    'Col_S':s.Col_S,'Col_T':s.Col_T,'Col_X':s.Col_X,'Col_P':s.Col_P,'Fitness':s.Fitness}
-
-    CDict = {'Name':c.Name,'HP':c.HP,'Shape':c.Shape,'Color':c.Color,'Opacity':c.Opacity,'Up':c.Up,
-    'Left':c.Left,'Right':c.Right,'Down':c.Down,'A1':c.A1,'A2':c.A2,'A3':c.A3,'A4':c.A4,'Col_C':c.Col_C,
-    'Col_S':c.Col_S,'Col_T':c.Col_T,'Col_X':c.Col_X,'Col_P':c.Col_P,'Fitness':c.Fitness}
-
-    TDict = {'Name':t.Name,'HP':t.HP,'Shape':t.Shape,'Color':t.Color,'Opacity':t.Opacity,'Up':t.Up,
-    'Left':t.Left,'Right':t.Right,'Down':t.Down,'A1':t.A1,'A2':t.A2,'A3':t.A3,'A4':t.A4,'Col_C':t.Col_C,
-    'Col_S':t.Col_S,'Col_T':t.Col_T,'Col_X':t.Col_X,'Col_P':t.Col_P,'Fitness':t.Fitness}
-
-    XDict = {'Name':x.Name,'HP':x.HP,'Shape':x.Shape,'Color':x.Color,'Opacity':x.Opacity,'Up':x.Up,
-    'Left':x.Left,'Right':x.Right,'Down':x.Down,'A1':x.A1,'A2':x.A2,'A3':x.A3,'A4':x.A4,'Col_C':x.Col_C,
-    'Col_S':x.Col_S,'Col_T':x.Col_T,'Col_X':x.Col_X,'Col_P':x.Col_P,'Fitness':x.Fitness}
-
-    PDict = {'Name':p.Name,'HP':p.HP,'Shape':p.Shape,'Color':p.Color,'Opacity':p.Opacity,'Up':p.Up,
-    'Left':p.Left,'Right':p.Right,'Down':p.Down,'A1':p.A1,'A2':p.A2,'A3':p.A3,'A4':p.A4,'Col_C':p.Col_C,
-    'Col_S':p.Col_S,'Col_T':p.Col_T,'Col_X':p.Col_X,'Col_P':p.Col_P,'Fitness':p.Fitness}
-
     with open("JsonTest.txt", "w+") as myfile:
-        json.dump(SDict,myfile,indent=4)
-
-    CurrentGame.C[1] =c
-    CurrentGame.S[1] = s
-    CurrentGame.T[1] = t
-    CurrentGame.X[1] = x
-    CurrentGame.Player[1] = p
-
+        json.dump(CurrentGame,myfile,default=dumper,indent=4)
 
 def main():
-    pop = InitializePopulation(10,0,None,3,15,100,100,1)
-    ToJson(pop[0])
+    pop = InitializePopulation(3,0,None,2,100,100,1)
+    pop[0].Fitness = 4
+    pop[1].Fitness = 1
+
+    ToJson(RecombineGame(pop[1],pop[0]))
 
 if __name__ == '__main__':
     main()
