4b0fa6d6ac8537848169d355c1841384f4329111
dmh989 <dmh989@mst.edu>
2017-03-19 23:57:47 -0500

    Added Unity global object project and DB connection code

---
+++ viz/LivingArcade/Assets/Scripts/DBUtils.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjController : MonoBehaviour
+{
+    public string gameURL = "http://149.56.28.102/display.php&ID=";
+
+    void Start()
+    {
+        string ID = "282931167881809444187161050873593334202";
+        StartCoroutine(GetScores(ID));
+    }
+
+    IEnumerator GetScores(string ID)
+    {
+        WWW obj_get = new WWW(gameURL + ID);
+        yield return obj_get;
+
+        if (obj_get.error != null)
+        {
+            print("There was an error getting the objects: " + obj_get.error);
+        }
+        else
+        {
+            print(obj_get.text);
+        }
+    }
+
+}
---
+++ viz/LivingArcade/Assets/Scripts/ObjectLogic.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectLogic : MonoBehaviour {
+    public string Shape;
+    public string Color;
+    public string Opacity;
+    public trigger[] trig;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		//Check for triggers
+        /*for(through this object's triggers)
+        {
+            //Check triggers
+            if (trigger is button held && unity says button is held)
+            {
+                //call director(nameofreaction) in order to check which reaction function to call and call it
+            }
+        }*/
+        
+	}
+
+    //Reactions
+    void DestroySelf()
+    {
+
+    }
+
+    void DestroyObj()
+    {
+
+    }
+
+    void CreateObj()
+    {
+
+    }
+
+    void CreateObjRad()
+    {
+
+    }
+
+    void Become()
+    {
+
+    }
+
+    void ModScore()
+    {
+
+    }
+
+    void ModXSpeed()
+    {
+
+    }
+
+    void ModYSpeed()
+    {
+
+    }
+
+    void SetXSpeed()
+    {
+
+    }
+
+    void SetYSpeed()
+    {
+
+    }
+
+    void ModColor()
+    {
+
+    }
+
+    void SetColor()
+    {
+
+    }
+
+    void ModOpacity()
+    {
+
+    }
+
+    void SetOpacity()
+    {
+
+    }
+
+    void ChangeShape()
+    {
+
+    }
+
+    void ChangeOffscreenEffect()
+    {
+
+    }
+}
